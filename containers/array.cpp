87b3b5bfc05212d1976113e598028ed8ee82754a
ecuadros <ecuadros@spc.org.pe>
2026-01-20 11:49:12 -0500

    Almost ready

--- containers/array.cpp
+++ containers/array.cpp
@@ -3,27 +3,52 @@ using namespace std;
 #include "array.h"
 
 CArray::CArray(size_t size){
-    m_size = size;
-    // m_data = new int[size];
+    m_capacity = size;
+    m_data = new value_type[size];
 }
 CArray::~CArray(){
-    // delete[] m_data;
+    delete[] m_data;
 }
 
 value_type& CArray::operator[](size_t index){
+    assert(index < m_capacity);
+    if( index > m_last )
+        m_last = index;
     return m_data[index];
 }
 
 size_t CArray::getSize() const {
-    return m_size;
+    return m_last;
 }
 
+void CArray::push_back(value_type value){
+    if( m_last >= m_capacity )
+        resize();
+    m_data[m_last++] = value;
+}
+
+void CArray::resize(){
+    size_t new_capacity = m_capacity + 10;
+    value_type* new_data = new value_type[new_capacity];
+    for(auto i = 0; i < m_capacity; ++i)
+        new_data[i] = m_data[i];
+    delete[] m_data;
+    m_data = new_data;
+    m_capacity = new_capacity;
+}
 
 void DemoArray(){
-    CArray arr1(10);
+    CArray arr1(5);
     for(auto i = 0; i < arr1.getSize(); ++i)
-        arr1[i] = i * 10;
+        arr1.push_back(i*10);
+    cout << "First part finished" << endl;
+    arr1[3] = 43;
+    arr1.push_back(80);
+    arr1.push_back(75);
 
+    cout << "Ready to print" << endl;
     for(auto i = 0; i < arr1.getSize(); ++i)
         cout << arr1[i] << endl;
+    // cout << arr1[50] << endl; // This will trigger an assertion failure
+    cout << "DemoArray finished." << endl;
 }
--- containers/array.h
+++ containers/array.h
@@ -6,14 +6,16 @@ using value_type = int;
 
 class CArray{
 private:
-    size_t  m_size;
-    value_type  m_data[100]; // Fixed size for simplicity;
+    size_t       m_capacity = 0, m_last = 0;
+    value_type  *m_data = nullptr;
 public:
     CArray(size_t size);
     ~CArray();
 
+    void push_back(value_type value);
     value_type& operator[](size_t index);
     size_t  getSize() const;
+    void resize();
 };
 
 void DemoArray();
